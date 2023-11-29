549a9267c3ca472259ade4cc9e6eb9f788691e6e
Francisco Gameiro <gameirofrancisco6@gmail.com>
2023-11-29 23:32:56 +0000

    Agora sim, é final!

--- js/functions.js
+++ js/functions.js
@@ -191,18 +191,18 @@ function btnClick()
 
 function generateBoxComponents()
 {
-    boxes.push(new Box(0 + 0 * 75, 400, 50, 50, null, battery, 'Battery', 225, 275))
-    boxes.push(new Box(0 + 1 * 75, 400, 50, 50, null, bulbOff, 'Bulb', 225, 175))
-    boxes.push(new Box(0 + 2 * 75, 400, 50, 50, null, resistor, 'Resistor', 175, 225))
-    boxes.push(new Box(0 + 3 * 75, 400, 50, 50, null, switchOn, 'Switch', 275, 225))
+    boxes.push(new Box(35 + 1 * 75, 400, 50, 50, null, battery, 'Battery', 225, 275))
+    boxes.push(new Box(35 + 2 * 75, 400, 50, 50, null, bulbOff, 'Bulb', 225, 175))
+    boxes.push(new Box(35 + 3 * 75, 400, 50, 50, null, resistor, 'Resistor', 175, 225))
+    boxes.push(new Box(35 + 4 * 75, 400, 50, 50, null, switchOn, 'Switch', 275, 225))
 }
 
 function generateBoxWires()
 {
-    boxes.push(new Box(0 + 0 * 75, 400, 50, 50, null, wireSL, 'Wire', 175, 175))
-    boxes.push(new Box(0 + 1 * 75, 400, 50, 50, null, wireSR, 'Wire', 275, 175))
-    boxes.push(new Box(0 + 2 * 75, 400, 50, 50, null, wireIL, 'Wire', 175, 275))
-    boxes.push(new Box(0 + 3 * 75, 400, 50, 50, null, wireIR, 'Wire', 275, 275))
+    boxes.push(new Box(0 + 2.75 * 70, 380, 50, 50, null, wireSL, 'Wire', 175, 175))
+    boxes.push(new Box(0 + 3.75 * 70, 380, 50, 50, null, wireSR, 'Wire', 275, 175))
+    boxes.push(new Box(0 + 2.75 * 70, 450, 50, 50, null, wireIL, 'Wire', 175, 275))
+    boxes.push(new Box(0 + 3.75 * 70, 450, 50, 50, null, wireIR, 'Wire', 275, 275))
 }
 
 function determinePoints()
